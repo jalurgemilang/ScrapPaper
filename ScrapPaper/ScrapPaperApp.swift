bf30e8146c2f97083421ea329d55463f5d5d6fe3
jalurgemilang <jalurgemilang@ringgit.me>
2025-05-06 11:39:24 +0800

    Initial Commit

---
+++ ScrapPaper/ScrapPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrapPaperApp.swift
+//  ScrapPaper
+//
+//  Created by Long Fong Yee on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrapPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
